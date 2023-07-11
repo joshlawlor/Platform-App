190c9697060d8c34bf365ef5fa41cdeee69ede3b
Josh Lawlor <joshlawlor1@gmail.com>
2023-07-11 18:05:44 -0400

    Added in some custom fonts to experiment with

--- platform-app/src/App.js
+++ platform-app/src/App.js
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Dungeons & Dummies</h1>
+      <br/>
         <img src={dagger} className="App-logo" alt="logo" />
       </header>
     </div>
