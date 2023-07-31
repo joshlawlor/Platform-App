45caba1a80be116b21defcf24239455f55f0c144
Josh Lawlor <joshlawlor1@gmail.com>
2023-07-31 14:48:26 -0700

    Basic Navbar added to AppHome

--- platform-app/src/pages/AppHome/AppHome.js
+++ platform-app/src/pages/AppHome/AppHome.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
-function AppHome(){
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+function AppHome() {
   const navigate = useNavigate();
-    return(
-        <div className="App">
-        <header className="App-header">
-        <h1 className='homeTitle'>Dungeons & Dummies</h1>
-        <br/>
-        </header>
-        <div>
-   
-        </div>
-      </div>
-    )
+  return (
+    <div className="App">
+      <Navbar />
+
+      <header className="App-header">
+        <h1 className="homeTitle">Dungeons & Dummies</h1>
+        <br />
+      </header>
+      <div></div>
+    </div>
+  );
 }
 
 export default AppHome;
