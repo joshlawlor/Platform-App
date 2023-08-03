87a95c39eb41dc799e49f9d0252a3ec8f9114dea
Josh Lawlor <joshlawlor1@gmail.com>
2023-08-03 12:12:16 -0700

    Login now runs getUser, displays username at homepage

--- platform-app/src/pages/AppHome/AppHome.js
+++ platform-app/src/pages/AppHome/AppHome.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import {getUser, resetUserSession} from '../../service/AuthService';
 function AppHome(){
+  const user = getUser();
+  console.log(user);
+  const name = user.username !== 'undefined' && user ? user.username : '';
   const navigate = useNavigate();
     return(
         <div className="App">
         <header className="App-header">
-        <h1 className='homeTitle'>Dungeons & Dummies</h1>
+        <h1 className='homeTitle'>Welcome {name}</h1>
         <br/>
         </header>
         <div>
