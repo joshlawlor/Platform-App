3a754119c133264a1383c1d74695506d382be716
Josh Lawlor <joshlawlor1@gmail.com>
2023-08-03 12:58:06 -0700

    Moved logout button to Navbar

--- platform-app/src/pages/AppHome/AppHome.js
+++ platform-app/src/pages/AppHome/AppHome.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { getUser } from "../../service/AuthService";
 import Navbar from "../Navbar/Navbar";
 function AppHome() {
+  const user = getUser();
+  const name = user.username !== "undefined" && user ? user.username : "";
   const navigate = useNavigate();
 
-  console.log(sessionStorage.user)
+  console.log(sessionStorage.user);
 
   return (
     <div className="App">
       <Navbar />
-
       <header className="App-header">
+        <h1>Welcome {name}</h1>
         <br />
       </header>
       <div></div>
--- platform-app/src/pages/Navbar/Navbar.js
+++ platform-app/src/pages/Navbar/Navbar.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
+import { getUser, resetUserSession } from "../../service/AuthService";
 
 export default function Navbar() {
   const navigate = useNavigate();
-
+  const user = getUser();
+  const logoutHandler = () => {
+    resetUserSession();
+    navigate("/login");
+  };
   return (
     <div className="navbar">
       <h1 className="navTitle">Dungeons & Dummies</h1>
       <button onClick={() => navigate("/home")}>Home</button>
       <button onClick={() => navigate("/profile")}>Profile</button>
       <button onClick={() => navigate("/chat")}>Chat</button>
+      <button onClick={logoutHandler}> LOGOUT</button>
     </div>
   );
 }
