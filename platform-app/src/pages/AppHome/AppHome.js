887d89ba432327cdda5fca09c3603fbc2cd4f560
Josh Lawlor <joshlawlor1@gmail.com>
2023-08-07 15:41:45 -0700

    Starting styling for castle wall element

--- platform-app/src/pages/AppHome/AppHome.js
+++ platform-app/src/pages/AppHome/AppHome.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { getUser, resetUserSession } from "../../service/AuthService";
 import Navbar from "../Navbar/Navbar";
+import "./AppHome.css";
+import { Card, Container } from "react-bootstrap";
 function AppHome() {
   const user = getUser();
   console.log(user);
@@ -11,13 +13,41 @@ function AppHome() {
   console.log(sessionStorage.user);
 
   return (
-    <div className="App">
+    <div className="AppHome">
       <Navbar />
       <header className="App-header">
         <h1>Welcome {name}</h1>
-        <br />
+        <div className="castle-wall-container">
+          <div className="rampart-container">
+            <div className="rampart">
+              <h1>rampart 1</h1>
+            </div>
+            <div className="rampart">
+              <h1>rampart 2</h1>
+            </div>
+            <div className="rampart">
+              <h1>rampart 2</h1>
+            </div>
+            <div className="rampart">
+              <h1>rampart 2</h1>
+            </div>
+            <div className="rampart">
+              <h1>rampart 2</h1>
+            </div>
+            <div className="rampart">
+              <h1>rampart 2</h1>
+            </div>
+          </div>
+
+          <div className="castle-wall">
+            <div className="forum-container">
+              <div className="forum-header">
+                <h2>Make a Post</h2>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
-      <div></div>
     </div>
   );
 }
