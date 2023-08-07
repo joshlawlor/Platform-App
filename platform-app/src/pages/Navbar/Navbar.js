d5bdf29a45debe3f35828f80623adeb4819db07f
Josh Lawlor <joshlawlor1@gmail.com>
2023-08-07 14:04:47 -0700

    Added styling to Nav buttons

--- platform-app/src/pages/Navbar/Navbar.js
+++ platform-app/src/pages/Navbar/Navbar.js
@@ -13,10 +13,10 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <h1 className="navTitle">Dungeons & Dummies</h1>
-      <button onClick={() => navigate("/home")}>Home</button>
-      <button onClick={() => navigate("/profile")}>Profile</button>
-      <button onClick={() => navigate("/chat")}>Chat</button>
-      <button onClick={logoutHandler}> LOGOUT</button>
+      <button class="btn" onClick={() => navigate("/home")}>Home</button>
+      <button class="btn" onClick={() => navigate("/profile")}>Profile</button>
+      <button class="btn" onClick={() => navigate("/chat")}>Chat</button>
+      <button class="btn" onClick={logoutHandler}> Logout</button>
     </div>
   );
 }
