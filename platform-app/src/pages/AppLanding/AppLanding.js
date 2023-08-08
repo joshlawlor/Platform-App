105f9a533b095bc0bd3cc7d476258396cad6717f
Josh Lawlor <joshlawlor1@gmail.com>
2023-08-08 14:07:37 -0700

    Moved castle wall to landing page. Working on animated background

--- platform-app/src/pages/AppLanding/AppLanding.js
+++ platform-app/src/pages/AppLanding/AppLanding.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import './AppLanding.css'
-import dagger from '../../components/images/dagger.png'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import "./AppLanding.css";
+import dagger from "../../components/images/dagger.png";
+import { useNavigate } from "react-router-dom";
 
-function AppLanding(){
+function AppLanding() {
   const navigate = useNavigate();
-    return(
-        <div className="App">
-        <header className="App-header">
-        <h1 className='homeTitle'>Dungeons & Dummies</h1>
-        <br/>
-        </header>
-        <img src={dagger} className="App-logo" alt="logo" />
-        <div>
-          <button onClick={() => navigate('/login')}>Login</button>
-          <button onClick={() => navigate('/register')}>Sign Up</button>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="homeTitle">Dungeons & Dummies</h1>
+      </header>
+      <div className="landing-castle-wall-container">
+        <div className="rampart-container">
+          <div className="landing-rampart"></div>
+          <div className="landing-rampart"></div>
+          <div className="landing-rampart"></div>
+          <div className="landing-rampart"></div>
+          <div className="landing-rampart"></div>
+        </div>
+        <div className="landing-castle-wall">
+          {/* <img src={dagger} className="App-logo" alt="logo" /> */}
+          <div>
+            <button onClick={() => navigate("/login")}>Login</button>
+            <button onClick={() => navigate("/register")}>Sign Up</button>
+          </div>
         </div>
       </div>
-    )
+    </div>
+  );
 }
 
 export default AppLanding;
