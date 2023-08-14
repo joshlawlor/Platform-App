2af0c4f16adac3163eab1c3cbea41b74f6edc9d0
Josh Lawlor <joshlawlor1@gmail.com>
2023-08-14 14:06:22 -0700

    Manual auth checks on each page now. Need to check token expiration now

--- platform-app/src/pages/ProfilePage/ProfilePage.js
+++ platform-app/src/pages/ProfilePage/ProfilePage.js
@@ -2,6 +2,11 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import { getUser } from "../../service/AuthService";
 function ProfilePage() {
+    if(getUser() == undefined){
+        console.log(getUser())
+        console.log('NO USER')
+        window.location.replace('/');
+      }
     const user = getUser();
     const name = user.username;
     return(
