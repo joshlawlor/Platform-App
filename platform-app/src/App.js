7a51576c4ebbcbfdb3635cb334518e01525e0b45
Josh Lawlor <joshlawlor1@gmail.com>
2023-08-15 17:30:51 -0700

    Token auth service for frontend added. In testing now

--- platform-app/src/App.js
+++ platform-app/src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 //AUTH IMPORTS
-import PublicRoute from "./routes/PublicRoute";
-import PrivateRoute from "./routes/PrivateRoute";
+// import PublicRoute from "./routes/PublicRoute";
+// import PrivateRoute from "./routes/PrivateRoute";
+import {getUser, getToken, setUserSession, resetUserSession} from "./service/AuthService";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
@@ -13,13 +15,44 @@ import AppLogin from "./pages/AppLogin/AppLogin";
 import AppRegister from "./pages/AppRegister/AppRegister";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import ChatPage from "./pages/ChatPage/ChatPage";
+
 //CONTEXT IMPORTS
 import SignUpProvider from "./context/SignUpProvider";
 export const AppContext = React.createContext({});
 
 
+const verifyTokenAPIURL = "https://iyufswqyje.execute-api.us-east-1.amazonaws.com/Development/verified"
+const apiKey = process.env.REACT_APP_API_KEY  
 
 function App() {
+
+  useEffect(() => {
+    const token = getToken();
+    if(token === 'undefined' || token === undefined || token === null || !token){
+      return;
+    }
+
+    const requestConfig = {
+      headers: { 'Content-Type': 'application/json',
+    'x-api-key': apiKey },
+    }
+
+    const requestBody = {
+      user: getUser(),
+      token: token
+    }
+
+    axios.post(verifyTokenAPIURL,requestBody,requestConfig).then(response => {
+      setUserSession(response.data.user, response.data.token)
+      console.log('Verified')
+    }).catch(() => {
+      console.log('Error Validating')
+      resetUserSession();
+    })
+
+
+  }, [])
+
   const AppContextValue = {};
 
   return (
