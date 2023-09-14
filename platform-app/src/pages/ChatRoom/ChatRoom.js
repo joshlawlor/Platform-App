81aa75a0b039a7978396031fc2712c136132dfcf
Josh Lawlor <joshlawlor1@gmail.com>
2023-09-13 19:28:37 -0700

    Chat Edit button functional. Edit form needs setup

--- platform-app/src/pages/ChatRoom/ChatRoom.js
+++ platform-app/src/pages/ChatRoom/ChatRoom.js
@@ -1,53 +1,94 @@
-import React, {useState, useEffect} from 'react'
-import Navbar from '../Navbar/Navbar'
-import { useLocation } from 'react-router-dom'
-import Chat from '../../components/Chat'
+import React, { useState, useEffect } from "react";
+import Navbar from "../Navbar/Navbar";
+import { useLocation } from "react-router-dom";
+import Chat from "../../components/Chat";
 import { auth, db } from "../../firebase";
-import { doc, deleteDoc, collection } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc, collection } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const ChatRoom = () => {
-    const [chatUser] = useAuthState(auth);
-    const [isOwner, setIsOwner] = useState(null)
-    const location = useLocation()
-    const roomID = location.state.id
-    const roomName  = location.state.name
-    
-    
+  const location = useLocation();
+  const roomID = location.state.id;
+  const roomName = location.state.name;
+  const roomOwner = location.state.owner;
+  const [chatUser] = useAuthState(auth);
+  const [isOwner, setIsOwner] = useState(null);
+  // const [chatTitle, setChatTitle] = useState(roomName);
+  const [showForm, setShowForm] = useState(false);
+ 
+
   useEffect(() => {
     if (chatUser === null) {
-      window.location.replace('/');
+      window.location.replace("/");
     }
-
-    const roomOwner = location.state.owner;
 
     if (roomOwner === chatUser.displayName) {
       setIsOwner(chatUser.displayName);
     }
   }, [chatUser]);
 
-  const chatRoomRef = doc(collection(db, 'chats'), roomID);
+  const chatRoomRef = doc(collection(db, "chats"), roomID);
   const deleteChatRoom = async () => {
     try {
       await deleteDoc(chatRoomRef);
-      window.location.replace('/chat');
+      window.location.replace("/chat");
     } catch (error) {
       console.error("Error deleting chat: ", error);
     }
   };
+  const editRoomName = async () => {
+    const newName = "EE";
 
+    try {
+      await updateDoc(chatRoomRef, {
+        name: newName,
+      });
+    } catch (error) {
+      console.error("Error editing chat: ", error);
+    }
+  };
+
+  const openEditForm = () => {
+    setShowForm(true);
+  };
+
+  const closeEditForm = () => {
+    setShowForm(false);
+  };
 
   return (
     <div>
-        <Navbar></Navbar>
-        <div>
-            <h1>CHAT ROOM:{roomName}</h1>
-            {isOwner ? <button onClick={deleteChatRoom} type="button">DELETE CHAT</button>: null}
-                      {chatUser ? <Chat roomName={roomName} roomID={roomID}/> : null}
+      <Navbar></Navbar>
+      <div>
+        <h1>CHAT ROOM:UNKOWN</h1>
+        {isOwner ? (
+          <button onClick={deleteChatRoom} type="button">
+            DELETE CHAT
+          </button>
+        ) : null}
+        {isOwner ? (
+          <button onClick={openEditForm} type="button">
+            EDIT CHAT
+          </button>
+        ) : null}
+        {showForm && (
+          <div>
+            <form>
+              <button type="button" onClick={closeEditForm}>
+                X
+              </button>
 
-        </div>
+              <label>New Room Name:</label>
+              <input type="text" />
+              <button type="button" onClick={editRoomName}>Save</button>
+              <br />
+            </form>
+          </div>
+        )}
+        {chatUser ? <Chat roomName={roomName} roomID={roomID} /> : null}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatRoom
+export default ChatRoom;
