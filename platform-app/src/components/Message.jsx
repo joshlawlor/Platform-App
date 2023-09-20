f295f4179b074d52fa8a1c05ffc13f1f5b35d25a
Josh Lawlor <joshlawlor1@gmail.com>
2023-09-20 15:54:54 -0700

    Messages now conditionally styled left/right depending on if they were sent/received

--- platform-app/src/components/Message.jsx
+++ platform-app/src/components/Message.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { auth } from "../firebase";
 import './Message.css'
 const Message = ({message}) => {
+  const messageAuthor = message.uid
+  const messageClass = 
+  messageAuthor === auth.currentUser.uid 
+  ? `${'message-sent'}`
+  : `${'message-received'}`
   return (
-    <div>
-        <div className='message-sent'>
+  
+        <div className={`${messageClass}`}>
             <p class="message-content">{message.name}</p>
             <p>{message.text}</p>
         </div>
 
-    </div>
+    
   )
 }
 
