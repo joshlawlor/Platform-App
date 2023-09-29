970890c924fc84600beec2cfc08ef7e6b09a954e
Josh Lawlor <joshlawlor1@gmail.com>
2023-09-28 20:19:05 -0700

    Moved castle-wall styling to Landing css. Setup container for campaign elements

--- platform-app/src/pages/AppHome/AppHome.js
+++ platform-app/src/pages/AppHome/AppHome.js
@@ -1,25 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { getUser } from "../../service/AuthService";
 import Navbar from "../Navbar/Navbar";
 import "./AppHome.css";
 function AppHome() {
-
-    const user = getUser();
-    const username = user ? user.username : "Guest";
-
-
-  
+  const user = getUser();
+  const username = user ? user.username : "Guest";
 
   return (
     <div className="AppHome">
       <Navbar />
       <header className="App-header">
-        {user ? <h1 className="home-title">Welcome {username} </h1> : <h1 className="home-title">Welcome</h1> }
-        <div className="castle-wall-container">
-          
-            
-          </div>
+        {user ? (
+          <h1 className="home-title">Welcome {username} </h1>
+        ) : (
+          <h1 className="home-title">Welcome</h1>
+        )}
+        <div className="campaign-menu">
+          <button>Create a Campaign</button>
+        </div>
       </header>
+      <div className="campaign-display-container">
+        <h1>Campaign containers</h1>
+      </div>
     </div>
   );
 }
